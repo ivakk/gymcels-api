d343ce99ccbd0eb2b5d332047db3c04c583cb697
Ivaylo Chevarganov <i503708@fontysict.nl>
2024-09-20 15:06:03 +0200

    Spring Boot project

---
+++ src/main/java/nl/fontys/s3/gymcels/GymcelsApplication.java
@@ -0,0 +1,13 @@
+package nl.fontys.s3.gymcels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymcelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymcelsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/fontys/s3/gymcels/GymcelsApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.fontys.s3.gymcels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymcelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
